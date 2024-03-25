3a34403926fb1652bef33b9b0ef82467f1b79a97
lazycoder578 <aknkhan2000@gmail.com>
2024-03-26 02:25:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {} from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-center'>Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
